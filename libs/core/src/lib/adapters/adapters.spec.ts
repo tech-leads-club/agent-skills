13124aa22f18f33a84c66c9ecd63cb940df1bd0b
Edmar Paulino <edmarpaulino9@gmail.com>
2026-03-06 12:41:52 -0300

    test(core): import jest globals in adapter specs

--- libs/core/src/lib/adapters/adapters.spec.ts
+++ libs/core/src/lib/adapters/adapters.spec.ts
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals'
 import { mkdtemp, readFile, rm } from 'node:fs/promises'
 import { createServer, type Server } from 'node:http'
 import { tmpdir } from 'node:os'
