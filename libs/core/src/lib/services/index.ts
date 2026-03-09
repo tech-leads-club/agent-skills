3a4b71bc604b2ef228cb770a9415bb7e3e09a18e
Edmar Paulino <edmarpaulino9@gmail.com>
2026-03-09 19:18:58 -0300

    feat(core): add lockfile read service

--- libs/core/src/lib/services/index.ts
+++ libs/core/src/lib/services/index.ts
@@ -1,3 +1,4 @@
+export * from './global-path.service'
+export * from './lockfile.service'
 export * from './markdown-parser.service'
 export * from './project-root.service'
-export * from './global-path.service'
