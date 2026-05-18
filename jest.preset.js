5d61652a59bab31f65677067a534d865fda9075e
STay HARD <chitkarauniversity436@gmail.com>
2026-05-18 23:02:17 +0530

    Update Jest preset for TypeScript and ESM support

--- jest.preset.js
+++ jest.preset.js
@@ -2,8 +2,29 @@ import nxPreset from '@nx/jest/preset.js'
 
 export default {
   ...nxPreset,
-  testMatch: ['**/__tests__/**/*.spec.ts', '**/+(*.)+(spec|test).ts'],
-  transform: { '^.+\\.[tj]s$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.spec.json' }] },
-  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
+
+  // Match all spec and test files
+  testMatch: [
+    '**/__tests__/**/*.spec.ts',
+    '**/+(*.)+(spec|test).ts',
+  ],
+
+  // Transform TypeScript files using ts-jest with ESM support
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.spec.json',
+      },
+    ],
+  },
+
+  // Fix relative imports ending with .js
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+
+  // Treat TypeScript files as ESM
   extensionsToTreatAsEsm: ['.ts'],
 }
