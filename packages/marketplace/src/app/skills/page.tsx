6e6af12e3ecd73f6eb64b2de97d3257828b20d2c
Waldemar Neto <waldemarnt@gmail.com>
2026-07-28 16:28:37 -0300

    feat(marketplace): add crawlable skills index on /skills

--- packages/marketplace/src/app/skills/page.tsx
+++ packages/marketplace/src/app/skills/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Suspense } from 'react'
 import { JsonLd } from '../../components/JsonLd'
+import { SkillsCrawlIndex } from '../../components/SkillsCrawlIndex'
 import marketplaceData from '../../data/skills.json'
 import { SkillsClient } from './SkillsClient'
 
@@ -44,6 +45,7 @@ export default function SkillsPage() {
       >
         <SkillsClient data={marketplaceData} />
       </Suspense>
+      <SkillsCrawlIndex skills={marketplaceData.skills} />
     </>
   )
 }
